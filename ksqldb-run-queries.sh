12584eabb480de4c1c5d5004be158e14657757a5
Martijn Kieboom <mkieboom@confluent.io>
2020-07-29 22:52:03 +0200

    Delete ksqldb-run-queries.sh

--- ksqldb-run-queries.sh
+++
@@ -1,4 +0,0 @@
-docker exec -i ksqldb-cli ksql -u QGL6R5MBMJRHV5GW -p TurUEYo/ELA1nCozFkPUkiIrTZ22Moj8vjD/r/4p2oz43Q9CeSW/FOINakTHXcUK https://pksqlc-42901.europe-west4.gcp.confluent.cloud:443 <<EOF
-RUN SCRIPT '/ksqlqueries.sql';
-exit
-EOF
