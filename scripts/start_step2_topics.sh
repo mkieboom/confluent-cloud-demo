5971d624a57b6491e3e2a135232a10a13cdac455
Martijn Kieboom <martijn@kieboom.nu>
2020-07-30 10:25:02 +0200

    Initial commit

--- scripts/start_step2_topics.sh
+++ scripts/start_step2_topics.sh
@@ -9,6 +9,8 @@ function create_kafka_avro_console_consumer {
   FILENAME=$2
 
   cat <<EOF > ${FILENAME}
+#!/bin/bash
+
 source ../env.sh
 
 kafka-avro-console-consumer \
@@ -29,6 +31,8 @@ function create_kafka_avro_console_consumer_frombeginning {
   FILENAME=$2
 
   cat <<EOF > ${FILENAME}
+#!/bin/bash
+
 source ../env.sh
 
 kafka-avro-console-consumer \
@@ -64,7 +68,49 @@ EOF
 #   chmod +x ${FILENAME}
 # }  
 
-# Create topics for the datagen sink connector
+
+function create_ksql_select_query {
+  KSQL_OBJECT_NAME=$1
+  FILENAME=$2
+
+
+cat <<EOFSH > ${FILENAME}
+#!/bin/bash
+
+source ../env.sh
+
+docker exec -i ksqldb-cli ksql -u ${KSQLDB_API_KEY} -p ${KSQLDB_API_SECRET} ${KSQLDB_ENDPOINT} <<EOF
+SET 'auto.offset.reset' = 'latest';
+SET 'commit.interval.ms'='1000';
+SELECT * FROM $KSQL_OBJECT_NAME EMIT CHANGES;
+EOF
+EOFSH
+
+  chmod +x ${FILENAME}
+}  
+
+function create_ksql_select_query_frombeginning {
+  KSQL_OBJECT_NAME=$1
+  FILENAME=$2
+
+
+cat <<EOFSH > ${FILENAME}
+#!/bin/bash
+
+source ../env.sh
+
+docker exec -i ksqldb-cli ksql -u ${KSQLDB_API_KEY} -p ${KSQLDB_API_SECRET} ${KSQLDB_ENDPOINT} <<EOF
+SET 'auto.offset.reset' = 'earliest';
+SET 'commit.interval.ms'='1000';
+SELECT * FROM $KSQL_OBJECT_NAME EMIT CHANGES;
+EOF
+EOFSH
+
+  chmod +x ${FILENAME}
+}  
+
+
+Create topics for the datagen sink connector
 ccloud kafka topic create customers --partitions 1 --if-not-exists
 ccloud kafka topic create products --partitions 1 --if-not-exists
 ccloud kafka topic create orders --partitions 1 --if-not-exists
@@ -90,8 +136,15 @@ create_kafka_avro_console_consumer_frombeginning products $EXAMPLE_FOLDER/2_kafk
 create_kafka_avro_console_consumer supplies $EXAMPLE_FOLDER/3_kafka-avro-console-consumer-supplies.sh
 create_kafka_avro_console_consumer orders $EXAMPLE_FOLDER/4_kafka-avro-console-consumer-orders.sh
 create_kafka_avro_console_consumer shoppingbasket $EXAMPLE_FOLDER/5_kafka-avro-console-consumer-shoppingbasket.sh
-
 create_kafka_avro_console_consumer product_supply_and_demand $EXAMPLE_FOLDER/6_kafka-avro-console-consumer-product_supply_and_demand.sh
-create_kafka_avro_console_consumer current_stock $EXAMPLE_FOLDER/7_kafka-avro-console-consumer-current_stock.sh
 
+create_kafka_avro_console_consumer current_stock $EXAMPLE_FOLDER/7a_kafka-avro-console-consumer-current_stock.sh
+create_ksql_select_query current_stock $EXAMPLE_FOLDER/7b_ksql-select-current_stock.sh
 
+create_kafka_avro_console_consumer total_order_value $EXAMPLE_FOLDER/8_kafka-avro-console-consumer-total_order_value.sh
+
+create_kafka_avro_console_consumer total_order_value_per_customer $EXAMPLE_FOLDER/9a_kafka-avro-console-consumer-total_order_value_per_customer.sh
+create_ksql_select_query total_order_value_per_customer $EXAMPLE_FOLDER/9b_ksql-select-total_order_value_per_customer.sh
+
+create_kafka_avro_console_consumer product_demand_last_3mins $EXAMPLE_FOLDER/10a_kafka-avro-console-consumer-product_demand_last_3mins.sh
+create_ksql_select_query product_demand_last_3mins $EXAMPLE_FOLDER/10b_ksql-select-product_demand_last_3mins.sh
