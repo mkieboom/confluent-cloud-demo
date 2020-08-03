55992b9b0c782c56f5fb01ef0588f87a37866edd
Martijn Kieboom <martijn@kieboom.nu>
2020-08-03 10:46:07 +0200

    Initial commit

--- scripts/destroy_step4_ccloud.sh
+++ scripts/destroy_step4_ccloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the latest ccloud_library shell script
-curl -sS https://raw.githubusercontent.com/confluentinc/examples/latest/utils/ccloud_library.sh > ./scripts/ccloud_library.sh
+#curl -sS https://raw.githubusercontent.com/confluentinc/examples/latest/utils/ccloud_library.sh > ./scripts/ccloud_library.sh
 
 # Load the Confluent Cloud library
 source ./scripts/ccloud_library.sh
@@ -9,6 +9,25 @@ source ./scripts/ccloud_library.sh
 # Source the ccloud environment file as created when launching the Confluent Cloud cluster
 source ./env.sh
 
+# Final check to make sure we deleted the cluster correctly
+CLUSTER_ID=$(ccloud::find_cluster $CLUSTER_NAME $CLUSTER_CLOUD $CLUSTER_REGION)
+if [ $? -eq 0 ]
+then
+	echo "Confluent Cloud cluster '" $CLUSTER_ID "' still exists!"
+else
+	echo "Confluent Cloud cluster '" $CLUSTER_ID "' successfully deleted."
+fi
+
 # Destroy the Confluent Cloud stack
 echo "Deleting Confluent Cloud stack with Service Account ID: ${SERVICE_ACCOUNT_ID}"
 ccloud::destroy_ccloud_stack $SERVICE_ACCOUNT_ID
+
+# Final check to make sure we deleted the cluster correctly
+# CLUSTER_ID=$(ccloud::find_cluster $CLUSTER_NAME $CLUSTER_CLOUD $CLUSTER_REGION)
+# if [ $? -eq 0 ]
+# then
+# 	echo "Confluent Cloud cluster '" $CLUSTER_ID "' still exists! Exiting now..."
+# 	exit;
+# else
+# 	echo "Confluent Cloud cluster '" $CLUSTER_ID "' successfully deleted."
+# fi
